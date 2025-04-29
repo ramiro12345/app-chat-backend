81ba4933b53418c8f38bd2c05e868978057b9211
Ramiro Quisbert <ramirocalderon490@gmail.com>
2025-04-28 21:56:01 -0400

    configuraciones para el despliegue

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config(
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors(
     {
       origin: ['https://chat-app-git-main-ramiros-projects-0e046e07.vercel.app/'],
       credentials: true,
     },
   );
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
